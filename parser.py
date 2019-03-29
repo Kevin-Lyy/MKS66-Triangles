069db734b28b1e84029d8943673d07fbae9514e7
Kevinlyy <batmanthelegendly@gmail.com>
2019-03-28 21:39:28 -0400

    parse

--- parser.py
+++ parser.py
@@ -133,6 +133,7 @@ def parse_file( fname, edges, polygons, transform, screen, color ):
 
         elif line == 'apply':
             matrix_mult( transform, edges )
+            matrix_mult(transform, polygons)
 
         elif line == 'clear':
             edges = []
@@ -141,6 +142,7 @@ def parse_file( fname, edges, polygons, transform, screen, color ):
         elif line == 'display' or line == 'save':
             clear_screen(screen)
             draw_lines(edges, screen, color)
+            draw_polygons(polygons, screen, color)
 
             if line == 'display':
                 display(screen)
